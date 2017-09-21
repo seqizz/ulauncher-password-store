336d36a64b3d8a8758d79b0dfb483b249ede995d
Gürkan Gür <gurkan.gur@innogames.com>
2017-09-21 18:31:35 +0200

    initial

---
+++ main.py
@@ -0,0 +1,43 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+import os
+
+class PassExtension(Extension):
+
+    def __init__(self):
+        super(PassExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = []
+    	pipe = os.popen("find ~/.password-store/ | sed '/gpg$/!d;s/.*.password-store\///;s/.gpg$//'")
+    	output = pipe.read()
+        myList = event.query.split(" ")
+        if len(myList) == 1:
+            for line in output.splitlines():
+                items.append(ExtensionResultItem(icon='images/key.png',
+                                                name='%s' % line,
+                                                description='Copy %s to clipboard' % line,
+                                                on_enter=RunScriptAction("pass show -c %s" % line, None)))
+
+            return RenderResultListAction(items)
+        else:
+            myQuery = myList[1]
+            for line in output.splitlines():
+                if myQuery in line:
+                    items.append(ExtensionResultItem(icon='images/key.png',
+                                                name='%s' % line,
+                                                description='Copy %s to clipboard' % line,
+                                                on_enter=RunScriptAction("pass show -c %s" % line, None)))
+
+            return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    PassExtension().run()
